bef5d559040083772871a4204348605b3d2c12f6
Victor Calado <134461266+VictorCalado13@users.noreply.github.com>
2023-10-19 16:39:00 -0300

    main.c

--- main.c
+++ main.c
@@ -6,11 +6,11 @@ int main()
   Tree t = createTree();
   // link x;
   // insert(t, 99);
-  // imprime(t, "oi");
+  // imprime(t, "arvore");
   // insert(t, 88);
-  // imprime(t, "oi");
+  // imprime(t, "arvore");
   // insert(t, 77);
-  // imprime(t, "oi");
+  // imprime(t, "arvore");
 
   AVLinsert(t, 18);
   AVLinsert(t, 2);
